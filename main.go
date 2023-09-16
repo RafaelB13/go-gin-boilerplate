43c9956fc6efcedb78abd925466ddb0f744f9eb4
Rafael Borges <rafaelborgesdev@gmail.com>
2023-09-16 18:49:02 -0300

    feat: user model config and init gorm on main.go

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"github.com/rafaelb13/go-gin-boilerplate/config"
 	app "github.com/rafaelb13/go-gin-boilerplate/service"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	//Init Config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("error starting settings", err)
+		return
+	}
 	// Start
 	app.ServerInitialize()
 }
--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type User struct {
+	gorm.Model
 	Id    string `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
