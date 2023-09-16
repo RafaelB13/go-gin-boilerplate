c52d3a31a22c6d999082215611641fa202c5ce9e
Rafael Borges <rafaelborgesdev@gmail.com>
2023-09-16 18:48:08 -0300

    🛠️ Adjust config and .gitignore

--- config/config.go
+++ config/config.go
@@ -1,8 +1,27 @@
 package config
 
 import (
+	"fmt"
 	"os"
+
+	"gorm.io/gorm"
 )
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	var err error
+	logger = GetLogger("config")
+	// Initialize MySQL
+	db, err = initMySQL()
+	if err != nil {
+		return fmt.Errorf("error initializing mysql: %v", err)
+	}
+	return nil
+}
 
 func ServerListenner() string {
 	DotEnv().LoadEnv()
@@ -25,4 +44,14 @@ func ServerListenner() string {
 
 	return appURL + port
 
+}
+
+func GetLogger(p string) *Logger {
+	// Initialize Logger
+	logger = NewLogger(p)
+	return logger
+}
+
+func GetDatabase() *gorm.DB {
+	return db
 }
